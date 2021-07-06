92743efff9766b015d88d0da4c01ccae6ff92201
Manvirender Singh Rawat <msrawat78@gmail.com>
2021-07-06 15:59:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('E:\\Manu\\projects\\Projects_Others\\MyWork\github\\msr-py-app-test\\stocks.csv')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="graph"),
+    html.Button("Switch Axis", id='btn', n_clicks=0)
+])
+
+@app.callback(
+    Output("graph", "figure"), 
+    [Input("btn", "n_clicks")])
+def display_graph(n_clicks):
+    if n_clicks % 2 == 0:
+        date, GOOG = 'date', 'GOOG'
+    else:
+        GOOG, date = 'GOOG', 'date'
+
+    fig = px.line(df, x=date, y=GOOG)    
+    return fig
+
+app.run_server(debug=True)
+
